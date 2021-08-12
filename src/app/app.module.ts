2f97a2de711ce580d940dcffc02c0f4274dcad3f
MadhusudhanReddyTippana <tippana.m.s.r@gmail.com>
2021-08-12 11:42:49 -0400

    Added wildcard route to intercept unknown url's and redirect to home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,8 @@ import { ForecastResolver } from './services/forecast-resolver.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path : 'forecast' , redirectTo : '' , pathMatch : 'full'},
-  { path: 'forecast/:zipcode', component: FivedayWeatherComponent, resolve: {forecast: ForecastResolver} }
+  { path: 'forecast/:zipcode', component: FivedayWeatherComponent, resolve: {forecast: ForecastResolver} },
+  { path: '**' , redirectTo : '' },
 ];
 
 @NgModule({
